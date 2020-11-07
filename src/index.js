1de56b1a9435a399ed9186cd4ec8ff6e1540bdf4
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-07 17:39:18 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.write("Welcome to Dominos!");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
+    res.end();
+  } else {
+    res.statusCode = 400;
+    res.write("Bad request");
+    res.end();
+  }
 }
 
 module.exports = httpServer;
